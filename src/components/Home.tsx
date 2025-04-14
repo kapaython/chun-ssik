75aee2cfb2fc3ee2fe897ad74a83f7b7e4eb5c7a
sure-why-not <pkse1213@gmail.com>
2025-04-14 19:13:36 +0900

    메인 화면 수정

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -17,36 +17,64 @@ const Home: React.FC = () => {
         {/* 설명 문장 */}
         <div>
           <p className="text-lg leading-relaxed mb-2">
-            안녕하세요. 우리팀 주번 춘씨익 :)이에요.<br/>
-            DB, 사용자의 앱 활동 로그, 기존 히스토리 분석하여 원인을 파악합니다.
+            고객센터에서 아래 고객문의가 인입되었어요 🕊️
           </p>
-        </div>
-
-        {/* 강조 문장 */}
-        <div>
-          <p className="font-semibold text-xl my-6">인입된 CS를 확인해볼까요?</p>
+          <p className="font-semibold text-xl my-6"> 💌 본인이 인지하지 못한 송금 문의 ・ 3건</p>
         </div>
 
         {/* 시작 버튼들 */}
-        <div className="flex flex-col gap-4 items-center">
-          <button
-            onClick={() => navigate('/conversation01')}
-            className="w-[70%] px-8 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors text-lg border border-blue-600"
-          >
-            💌 미인지 송금 확인 요청 케이스 1
-          </button>
-          <button
-            onClick={() => navigate('/conversation02')}
-            className="w-[70%] px-8 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors text-lg border border-blue-600"
-          >
-            💌 미인지 송금 확인 요청 케이스 2
-          </button>
-          <button
-            onClick={() => navigate('/conversation03')}
-            className="w-[70%] px-8 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors text-lg border border-blue-600"
-          >
-            💌 미인지 송금 확인 요청 케이스 3
-          </button>
+        <div className="flex flex-col gap-4 items-center w-full text-left">
+          <div className="w-full max-w-2xl bg-gray-50 rounded-xl p-6">
+            <div className="flex justify-between items-center">
+              <div className="flex items-start">
+                <div className="ml-2">
+                  <p className="font-semibold text-gray-800 mt-1">내용</p>  
+                  <p className="font-semiboldtext-gray-800 mt-1">2024년 4월 1일 13시에 김응수에게 3건 송금한 기록이 있는데, 저는 이런 송금을 한 기억이 없습니다.</p>
+                  <p className="font-semibold text-gray-600">페이어카 : 10001234</p>
+                </div>
+              </div>
+              <button
+                onClick={() => navigate('/conversation01')}
+                className="bg-[#6B7AED] text-white px-8 py-2 rounded-full hover:bg-blue-600 transition-colors text-sm flex-shrink-0 ml-4 self-center border border-[#6B7AED]"
+              >
+                해결하기
+              </button>
+            </div>
+          </div>
+          <div className="w-full max-w-2xl bg-gray-50 rounded-xl p-6">
+            <div className="flex justify-between items-center">
+              <div className="flex items-start">
+                <div className="ml-2">
+                  <p className="font-semibold text-gray-800 mt-1">내용</p>  
+                  <p className="font-semiboldtext-gray-800 mt-1">제가 진행하지 않은 2024년 4월 5일 8시 이*지 1,000,000원이 신한은행 계좌로송금 되었어요.</p>
+                  <p className="font-semibold text-gray-600">페이어카 : 43214321</p>
+                </div>      
+              </div>
+              <button
+                onClick={() => navigate('/conversation03')}
+                className="bg-[#6B7AED] text-white px-8 py-2 rounded-full hover:bg-blue-600 transition-colors text-sm flex-shrink-0 ml-4 self-center border border-[#6B7AED]"
+              >
+                해결하기
+              </button>
+            </div>
+          </div>
+          <div className="w-full max-w-2xl bg-gray-50 rounded-xl p-6">
+            <div className="flex justify-between items-center">
+              <div className="flex items-start">
+                <div className="ml-2">
+                  <p className="font-semibold text-gray-800 mt-1">내용</p>  
+                  <p className="font-semiboldtext-gray-800 mt-1">2024년 12월 29일 카카오페이로 제 국민은행 계좌로 10만원이 송금되었는데, 저는 송금한적이 없어요. 이거 해킹 된건가요?</p>
+                  <p className="font-semibold text-gray-600">페이어카 : 11112222</p>
+                </div>
+              </div>
+              <button
+                onClick={() => navigate('/conversation02')}
+                className="bg-[#6B7AED] text-white px-8 py-2 rounded-full hover:bg-blue-600 transition-colors text-sm flex-shrink-0 ml-4 self-center border border-[#6B7AED]"
+              >
+                해결하기
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
